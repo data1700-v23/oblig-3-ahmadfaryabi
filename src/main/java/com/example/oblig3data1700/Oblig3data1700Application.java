deb07abba77b8403aa8a3ef00d8498fd2dc9bcda
ahmadfaryabi <111738875+ahmadfaryabi@users.noreply.github.com>
2023-04-26 13:38:45 +0200

    endring i filene

--- src/main/java/com/example/oblig3data1700/Oblig3data1700Application.java
+++ src/main/java/com/example/oblig3data1700/Oblig3data1700Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Oblig3data1700Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Oblig3data1700Application.class, args);
     }
